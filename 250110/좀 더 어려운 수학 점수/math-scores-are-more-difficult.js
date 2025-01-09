e58ccb18d915d720bfa0ee1e1eb98a11d1de41fa
황다경 <100592495+Monixc@users.noreply.github.com>
2025-01-10 00:40:30 +0900

    Title: 좀 더 어려운 수학 점수, Time: 166ms, Memory: 14MB, Status: Runtime Error - CodeTree

--- 250110/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
+++ 250110/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -3,15 +3,8 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 const [am, ae] = input[0].split(" ").map(Number);
 const [bm, be] = input[1].split(" ").map(Number);
 
-
-if(am !== bm && am > bm){
+if(am > bm || (am === bm && ae > eb)){
     console.log("A");
-}else if(am !== bm && am < bm){
-    console.log("B");
-}
-
-if((am === bm) && ae > be){
-    console.log("A");
-}else if((am === bm) && ae < be){
+}else{
     console.log("B");
 }
