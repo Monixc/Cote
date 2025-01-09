1d3f7917f09053ef8a996e3067508c8a70398eaf
황다경 <100592495+Monixc@users.noreply.github.com>
2025-01-09 23:36:33 +0900

    Title: 굉장한 수, Time: 182ms, Memory: 15MB, Status: Passed - CodeTree

--- 250109/굉장한 수/amazing-number.js
+++ 250109/굉장한 수/amazing-number.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 let input = Number(fs.readFileSync(0).toString().trim());
 
-if((input % 3 === 0 && input % 2 === 1) || (input % 5 === 0 && input % 2 === 1)){
+if((input % 3 === 0 && input % 2 === 1) || (input % 5 === 0 && input % 2 === 0)){
     console.log("true");
 }else{
     console.log("false");
