4c86c17ee8779d1b4797bcd38f93314fc34c5b69
황다경 <100592495+Monixc@users.noreply.github.com>
2025-01-09 23:31:49 +0900

    Title: 범위 밖의 수, Time: 187ms, Memory: 15MB, Status: Passed - CodeTree

--- 250109/범위 밖의 수/number-out-of-range.js
+++ 250109/범위 밖의 수/number-out-of-range.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 let input = Number(fs.readFileSync(0).toString().trim());
 
-if(a < 10 || a > 20){
+if(input < 10 || input > 20){
     console.log("yes");
 }else{
     console.log("no");
