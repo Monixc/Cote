bd6866c49da40c9d8685dc2eba568ad2d4115c24
황다경 <100592495+Monixc@users.noreply.github.com>
2025-01-10 00:41:24 +0900

    Title: 좀 더 어려운 수학 점수, Time: 172ms, Memory: 15MB, Status: Passed - CodeTree

--- 250110/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
+++ 250110/좀 더 어려운 수학 점수/math-scores-are-more-difficult.js
@@ -3,7 +3,7 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 const [am, ae] = input[0].split(" ").map(Number);
 const [bm, be] = input[1].split(" ").map(Number);
 
-if(am > bm || (am === bm && ae > eb)){
+if(am > bm || (am === bm && ae > be)){
     console.log("A");
 }else{
     console.log("B");
