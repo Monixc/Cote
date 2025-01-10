bb86526e4a4fb94dba49bd7916f070b5f5d2ada8
황다경 <100592495+Monixc@users.noreply.github.com>
2025-01-10 14:22:25 +0900

    Title: 일 수 구하기, Time: 183ms, Memory: 15MB, Status: Passed - CodeTree

--- 250110/일 수 구하기/number-of-days-in-month.js
+++ 250110/일 수 구하기/number-of-days-in-month.js
@@ -1,17 +1,22 @@
 const fs = require("fs");
 let year = Number(fs.readFileSync(0).toString().trim());
 
-if (!((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
-    if(year=== 2){
-        console.log(28);
-    }
-    else if(year === 8){
+if (year === 2) {
+    console.log(28);
+}
+else if (year <= 7) {
+    if (year % 2 === 1) {
         console.log(31);
     }
-    else if(year % 2 === 0 ){
+    else {
         console.log(30);
     }
-    else{
+}
+else {
+    if (year % 2 === 0) {
         console.log(31);
+    }
+    else {
+        console.log(30);
     }
 }
