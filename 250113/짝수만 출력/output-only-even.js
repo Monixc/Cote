a22660c13cd4f8e9981e6bf268084d1eb6ebe196
황다경 <100592495+Monixc@users.noreply.github.com>
2025-01-13 17:17:39 +0900

    Title: 짝수만 출력, Time: 204ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250113/짝수만 출력/output-only-even.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+let [a, b] = input;
+let result = "";
+
+while(a <= b){
+    if(a % 2 === 0){
+        result += (a + " ");
+    }
+    a++;
+}
+
+console.log(result);
