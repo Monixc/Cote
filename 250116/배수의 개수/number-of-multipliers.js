6defd7d1797f6b72646858b0d4a9571ef58ea7dc
황다경 <100592495+Monixc@users.noreply.github.com>
2025-01-16 20:12:31 +0900

    Title: 배수의 개수, Time: 197ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250116/배수의 개수/number-of-multipliers.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n").map(Number);
+let cnt1 = 0; 
+let cnt2 = 0;
+
+for(let i = 0; i < 10; i++){
+    if(input[i] % 3 == 0){
+        cnt1++;
+    }
+    if(input[i] % 5 === 0){
+        cnt2++;
+    }
+}
+console.log("%d %d", cnt1, cnt2);
