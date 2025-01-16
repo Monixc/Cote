de43cc12f3f2be72121fc8eb03a05676e179dc83
황다경 <100592495+Monixc@users.noreply.github.com>
2025-01-16 20:04:09 +0900

    Title: a 부터 b 까지, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

--- 250116/a 부터 b 까지/a-to-b.js
+++ 250116/a 부터 b 까지/a-to-b.js
@@ -1,15 +1,17 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
-let [a, b] = input;
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+
 let result = "";
-for(let i = a; i <= b; i++){
-    if(i%2===0){
-        i += 3;
-        result += (i + " ");
+while (a <= b) {
+    result += a + " ";
+    if (a % 2 === 1) {
+        a *= 2;
     }
-    else if(i%2 === 1){
-        i *= 2;
-        result += (i+ " ");
+    else {
+        a += 3;
     }
 }
+
 console.log(result);
