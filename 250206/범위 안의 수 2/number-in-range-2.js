ee5ef892da7661bc2c1ac3f37b3a82428294b68d
황다경 <100592495+Monixc@users.noreply.github.com>
2025-02-06 14:16:37 +0900

    Title: 범위 안의 수 2, Time: 186ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250206/범위 안의 수 2/number-in-range-2.js
+++ 250206/범위 안의 수 2/number-in-range-2.js
@@ -2,9 +2,8 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n").map(Number);
 let sum = 0;
 let cnt = 0;
-let len = input.length;
 
-for(let i = input[0]; i <= len; i++){
+for(let i = 0; i <= 9; i++){
     if(input[i] >= 0 && input[i] <= 200){
         sum += input[i];
         cnt++;
