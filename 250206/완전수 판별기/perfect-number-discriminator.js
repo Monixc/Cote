b21e5daa4b50f2a80f17b6bf8569de6f2d51bf22
황다경 <100592495+Monixc@users.noreply.github.com>
2025-02-06 14:59:11 +0900

    Title: 완전수 판별기, Time: 169ms, Memory: 15MB, Status: Passed - CodeTree

--- 250206/완전수 판별기/perfect-number-discriminator.js
+++ 250206/완전수 판별기/perfect-number-discriminator.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 let sum = 0;
 
-for(let i = 1; i <= n; i++){
+for(let i = 1; i < n; i++){
     if(n % i === 0){
         sum += i;
     }
