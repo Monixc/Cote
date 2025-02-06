9179e5af49173ea95d44d9ef2c533bd6ba87e52c
황다경 <100592495+Monixc@users.noreply.github.com>
2025-02-06 15:32:47 +0900

    Title: 특정 조건의 온전수 구하기, Time: 166ms, Memory: 15MB, Status: Passed - CodeTree

--- 250206/특정 조건의 온전수 구하기/print-onjeonsu.js
+++ 250206/특정 조건의 온전수 구하기/print-onjeonsu.js
@@ -3,7 +3,7 @@ let n = Number(fs.readFileSync(0).toString().trim());
 let arr = "";
 
 for(let i = 1; i <= n; i++){
-    if(i % 2 === 0 || i % 5 === 0 || (i % 3 === 0 && i % 9 !== 0)){
+    if(i % 2 === 0 || i % 10 === 5 || (i % 3 === 0 && i % 9 !== 0)){
         continue;
     }
     arr += (i + " ");
