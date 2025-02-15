453176c7a7e0e77dc14957c0e27c9891f775e5e9
황다경 <100592495+Monixc@users.noreply.github.com>
2025-02-16 02:47:34 +0900

    Title: 3N + 1 수열, Time: 191ms, Memory: 15MB, Status: Passed - CodeTree

--- 250216/3N + 1 수열/3n-plus-1-sequence.js
+++ 250216/3N + 1 수열/3n-plus-1-sequence.js
@@ -3,18 +3,20 @@ let N = Number(fs.readFileSync(0).toString().trim());
 let cnt = 0;
 
 while(true){
-    if(N % 2 === 0){
-        N /= 2;
-        cnt++;
-    }
-    else if(N % 2 === 1){
-        N = N * 3 + 1;
-        cnt++;
-    }
     
     if(N === 1){
         console.log(cnt);
         break;
+    }
+    else{
+        if(N % 2 === 0){
+        N /= 2;
+        cnt++;
+        }
+        else if(N % 2 === 1){
+            N = N * 3 + 1;
+            cnt++;
+        }
     }
     
 }
