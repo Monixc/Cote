4b29eebbe47a2731a2ff54580ee7812052216bcf
황다경 <100592495+Monixc@users.noreply.github.com>
2025-04-01 22:34:42 +0900

    Title: Date to Date, Time: 177ms, Memory: 15MB, Status: Passed - Codetree

--- 250401/Date to Date/date-to-date.js
+++ 250401/Date to Date/date-to-date.js
@@ -3,17 +3,18 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
 
-// Please Write your code here.
-const month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+function numOfDays(m, d){
+    let total_days = 0;
+    const days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-let start_month = 0;
-let end_month = 0;
+    for(let i = 1; i < m; i++){
+        total_days += days[i];
+    }
 
-for(let i = 0; i <= m1; i++){
-    start_month += month[i];
+    total_days += d;
+
+    return total_days;
 }
 
-for(let i = 0; i <= m2; i++){
-    end_month += month[i];
-}
-console.log((end_month + d2) - (start_month + d1));
+const total_days = numOfDays(m2, d2) - numOfDays(m1, d1) + 1;
+console.log(total_days);
