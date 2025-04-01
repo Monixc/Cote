9ebc4ea98a623b1e6a8cbfaea4c9bc3bdb63b2f6
황다경 <100592495+Monixc@users.noreply.github.com>
2025-04-01 23:33:33 +0900

    Title: 그 요일은, Time: 165ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250401/그 요일은/the-day-of-the-day.js
+++ 250401/그 요일은/the-day-of-the-day.js
@@ -2,13 +2,20 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+
+//요일
 const A = input[1];
 
+//요일 배열
 const day = ["Mon", "Tue", "Web", "Thu", "Fri", "Sat", "Sun"];
+
+//월별 날짜수 
 const days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+//A에 입력된 날짜의 day 인덱스 
 let indexOfDay = 0;
 
-// Please Write your code here.
+//입력된 날(월, 일)을 일수로 변환
 function numOfDays(m, d){
     totalDays = 0;
     
@@ -20,12 +27,14 @@ function numOfDays(m, d){
 }
 
 
-
+//A와 일치하는 배열 요소를 찾아서, indexOfDay 변수에 저장
+//예: A가 Sat이라면 5(day[5]) + 1
 for(let i = 0; i <= day.length; i++){
     if(A === day[i]){
-        indexOfDay = i+1;
+        indexOfDay = i + 1;
     }
 }
 
-const diff = numOfDays(m2, d2) - numOfDays(m1, d1);
-console.log(parseInt((diff + indexOfDay) / 7));
+//시작일과 종료일 간 차이를 계산
+const diff = numOfDays(m2, d2) - numOfDays(m1, d1) + indexOfDay;
+console.log(parseInt(diff / 7));
