a2fb1a50996f090b771ce378dcf93b556b0d52f4
황다경 <100592495+Monixc@users.noreply.github.com>
2025-04-01 23:23:12 +0900

    Title: 그 요일은, Time: 178ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250401/그 요일은/the-day-of-the-day.js
+++ 250401/그 요일은/the-day-of-the-day.js
@@ -23,9 +23,9 @@ function numOfDays(m, d){
 
 for(let i = 0; i <= day.length; i++){
     if(A === day[i]){
-        indexOfDay = i - 1;
+        indexOfDay = i;
     }
 }
 
 const diff = numOfDays(m2, d2) - numOfDays(m1, d1);
-console.log(parseInt((diff + indexOfDay) / 7));
+console.log(parseInt((diff + indexOfDay) / 7 + 1));
