b76872a190765bd6468c9835d37db520c296eeea
황다경 <100592495+Monixc@users.noreply.github.com>
2025-04-01 23:39:40 +0900

    Title: 그 요일은, Time: 169ms, Memory: 15MB, Status: Passed - Codetree

--- 250401/그 요일은/the-day-of-the-day.js
+++ 250401/그 요일은/the-day-of-the-day.js
@@ -10,7 +10,7 @@ const A = input[1];
 const day = ["Mon", "Tue", "Web", "Thu", "Fri", "Sat", "Sun"];
 
 //월별 날짜수 
-const days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 //A에 입력된 날짜의 day 인덱스 
 let indexOfDay = 0;
@@ -28,8 +28,7 @@ function numOfDays(m, d){
 
 
 //A와 일치하는 배열 요소를 찾아서, indexOfDay 변수에 저장
-//예: A가 Sat이라면 5(day[5]) + 1
-for(let i = 0; i <= day.length; i++){
+for(let i = 0; i < day.length; i++){
     if(A === day[i]){
         indexOfDay = i;
     }
