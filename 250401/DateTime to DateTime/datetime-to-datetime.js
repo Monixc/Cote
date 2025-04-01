8d9b25fe776af1bcc66db92e882ef563d8d6cac4
황다경 <100592495+Monixc@users.noreply.github.com>
2025-04-01 22:40:14 +0900

    Title: DateTime to DateTime, Time: 174ms, Memory: 15MB, Status: Passed - Codetree

--- 250401/DateTime to DateTime/datetime-to-datetime.js
+++ 250401/DateTime to DateTime/datetime-to-datetime.js
@@ -7,5 +7,10 @@ const [A, B, C] = input[0].split(' ').map(Number);
 let start = 11 * 30 * 24 * 60 + 11 * 24 * 60 + 11 * 60 + 11;
 let end = 11 * 30 * 24 * 60 + A * 24 * 60 + B * 60 + C;
 
-console.log(end - start);
+if(start > end){
+    console.log(-1);
+}
+else{
+    console.log(end - start);
+}
 
