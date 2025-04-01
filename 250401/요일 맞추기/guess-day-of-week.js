7ca292b4e23b61be2295ff9644687582722c323d
황다경 <100592495+Monixc@users.noreply.github.com>
2025-04-01 22:59:36 +0900

    Title: 요일 맞추기, Time: 187ms, Memory: 15MB, Status: Wrong Answer - Codetree

---
+++ 250401/요일 맞추기/guess-day-of-week.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+// Please Write your code here.
+const day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri' , 'Sat', 'Sun'];
+const days_of_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function numOfDays(m, d) {
+    let total_days = 0;
+    
+    for(let i = 1; i < m; i++){
+        total_days += days_of_month[i];
+    }
+
+    return total_days + d;
+}
+
+function getElementByIndex(arr, index){
+    if(index >= 0){
+        return arr[index];
+    }
+
+    else{
+        const reverseIndex = arr.length + index;
+        return arr[reverseIndex];
+    }
+}
+
+const numOfStart = numOfDays(m1, d1);
+const numOfEnd = numOfDays(m2, d2);
+
+console.log(getElementByIndex(day, numOfEnd-numOfStart));
